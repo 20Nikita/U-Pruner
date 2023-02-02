f64711bc1e4f539a2954f53f2fa4e3300e6c2c86
@Nikita <gordeev.2017@bk.ru>
2023-02-02 16:54:28 +0300

    Added support for pruning after an already cropped model

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
 import yaml
+config = yaml.safe_load(open('Pruning.yaml'))
+os.environ["CUDA_VISIBLE_DEVICES"] = "{}".format(config['nni_pruning']['gpu'])
+
 import torch
 import copy
 import time
@@ -19,8 +23,6 @@ component = 'pruning'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-config = yaml.safe_load(open('Pruning.yaml'))
-
 snp = config['path']['exp_save'] + "/" + config['path']['model_name']
 if not os.path.exists(snp): os.makedirs(snp)
 
@@ -40,7 +42,7 @@ if config['algorithm'] == 'My_pruning':
 else:
     model = model.to(device)
     traced_optimizer = nni.trace(torch.optim.Adam)(model.parameters())
-    config_list = [{ 'sparsity': config[config['algorithm']]['P'], 'op_types': ['Conv2d'] }]
+    config_list = [{ 'sparsity': config['nni_pruning']['P'], 'op_types': ['Conv2d'] }]
     pruning_params = {
         'trainer': trainer.trainer,
         'traced_optimizer': traced_optimizer,
@@ -55,11 +57,11 @@ else:
     elif  config['algorithm'] == 'TaylorFOWeight':
         pruner = TaylorFOWeightPruner(model, config_list, trainer.trainer, traced_optimizer, trainer.criterion, training_batches=config['dataset']['dataLoader']['batch_size_t'])
     elif  config['algorithm'] == 'AGP':
-        pruner = AGPPruner(model, config_list, pruning_algorithm='taylorfo', total_iteration=config['AGP']['total_iteration'], finetuner=trainer.finetuner, log_dir = snp, pruning_params = pruning_params)
+        pruner = AGPPruner(model, config_list, pruning_algorithm='taylorfo', total_iteration=config['nni_pruning']['total_iteration'], finetuner=trainer.finetuner, log_dir = snp, pruning_params = pruning_params)
     elif  config['algorithm'] == 'Linear':
-        pruner = LinearPruner(model, config_list, pruning_algorithm='taylorfo', total_iteration=config['Linear']['total_iteration'], finetuner=trainer.finetuner, log_dir = snp, pruning_params = pruning_params)
+        pruner = LinearPruner(model, config_list, pruning_algorithm='taylorfo', total_iteration=config['nni_pruning']['total_iteration'], finetuner=trainer.finetuner, log_dir = snp, pruning_params = pruning_params)
     elif  config['algorithm'] == 'LotteryTicket':
-        pruner = LotteryTicketPruner(model, config_list, pruning_algorithm='taylorfo', total_iteration=config['LotteryTicket']['total_iteration'], finetuner=trainer.finetuner, log_dir = snp, pruning_params = pruning_params)
+        pruner = LotteryTicketPruner(model, config_list, pruning_algorithm='taylorfo', total_iteration=config['nni_pruning']['total_iteration'], finetuner=trainer.finetuner, log_dir = snp, pruning_params = pruning_params)
     if config['algorithm'] == 'L2Norm' or config['algorithm'] == 'FPGM' or config['algorithm'] == 'TaylorFOWeight':
         masked_model, masks = pruner.compress()
     else:
@@ -67,7 +69,7 @@ else:
         _, model, masks, _, _ = pruner.get_best_result()
     pruner._unwrap_model()
     model = ModelSpeedup(model, masks).to(device)
-    if config['L2Norm']['training']:
+    if config['nni_pruning']['training']:
         optimizer = optim.Adam(model.parameters(), lr=config['training']['lr'])
         model, loss, acc, st, time_elapsed2 = trainer.trainer(model, optimizer, trainer.criterion, num_epochs = config['training']['num_epochs'])    
          
@@ -78,7 +80,13 @@ stract2 = get_stract(model_prun)
 
 size = get_size(copy.deepcopy(model_prun)) / get_size(copy.deepcopy(model_orig))
 params = model_prun.state_dict()
-resize = []
+if ('pruning' in interface) and len(interface['pruning']['summary']['resize']):
+    resize = interface['pruning']['summary']['resize']
+else:
+    resize = []
+    interface['pruning'] = {}
+    interface['pruning']['summary'] = {}
+    interface['pruning']['is_pruning'] = True
 for i in range(len(stract1)):
     if stract1[i][2]!=stract2[i][2]:
         resize.append({ "name": rename(stract1[i][0]), "type": stract1[i][1], "orig_shape": stract1[i][2],"shape": stract2[i][2]})
@@ -90,7 +98,5 @@ summary = {
         'resize': resize,
         'time': "{:.0f}h {:.0f}m {:.0f}s".format(time_elapsed // 60 // 60, time_elapsed // 60 - time_elapsed // 60 // 60 * 60, time_elapsed % 60)
 }
-interface['pruning'] = {}
-interface['pruning']['summary'] = {}
-interface['pruning']['is_pruning'] = True
-save_interface(interface=interface, name_component=component, params=params, summary=summary)
+
+save_interface(path_to_interface = config['model']['path_to_interface'], interface=interface, name_component=component, params=params, summary=summary)
